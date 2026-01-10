0edbb8d729c2b6512f31bbd894352b53f324d667
Adrian R. Esguerra <i.am.adriansgrrx@gmail.com>
2026-01-10 15:16:59 +0800

    feat: add user authentication flow with Login and CreateAccount components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,44 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
 import { Route, Routes } from 'react-router'
 import HomePage from './pages/HomePage'
 import CreatePage from './pages/CreatePage'
 import NoteDetailsPage from './pages/NoteDetailsPage'
+import Login from './pages/Login'
+import CreateAccount from './pages/CreateAccount'
+import axios from 'axios';
 import toast from 'react-hot-toast'
 
 const App = () => {
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        try {
+          const res = await axios.get('api/users/me', {
+            headers: {Authorization: `Bearer ${token}`}
+          })
+          setUser(res.data);
+        } catch (error) {
+          setError("Failed to fetch data.");
+          localStorage.removeItem("token");
+        }
+      }
+    };
+    fetchUser();
+  }, []);
+
   return (
     <div data-theme='retro'>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/" element={<CreateAccount />} />
         <Route path="/create" element={<CreatePage />} />
         <Route path="/note/:id" element={<NoteDetailsPage />} />
+
       </Routes>
     </div>
   )
